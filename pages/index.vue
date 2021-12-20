f9e8f31dfc91b7dd9c9909fc2a91ae7129e658dd
朱煜乾 <416556102@qq.com>
2021-12-20 20:17:14 +0800

    add：新增本月热播

--- pages/index.vue
+++ pages/index.vue
@@ -5,32 +5,38 @@
     <content-warp name="片单推荐" size="small" more-link="/album">
       <album-item v-for="album of recommendedAlbumList" :key="album.albumId" :info="album" :card="false"/>
     </content-warp>
-    <content-warp :name="$t('Hot')" size="small" more-link="/show">
+    <content-warp name="本月热播" size="small" more-link="/show">
+      <show-item shape="list" v-for="(show,index) of monthShowList" :key="show.showId" :info="show" :delay="index"/>
+    </content-warp>
+    <content-warp name="高分推荐" size="small" more-link="/show">
       <show-item v-for="(show,index) of hotShowList" :key="show.showId" shape="list" :info="show" :delay="index"/>
     </content-warp>
-    <content-warp :name="$t('Will Return')" size="small" more-link="/show">
+    <content-warp name="即将回归" size="small" more-link="/show">
       <show-item v-for="(show,index) of returnShowList" :key="show.showId" shape="list" :info="show" :delay="index"/>
     </content-warp>
   </div>
   <!--pc-->
   <el-row v-else :gutter="15">
     <el-col :xs="15" :sm="15" :md="17" :lg="18" :xl="19">
-      <content-warp :name="$t('Hot')" size="small" more-link="/show">
-        <el-row>
-          <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="8" v-for="(show,index) of hotShowList" :key="show.showId">
-            <show-item shape="list" :info="show" :delay="index"/>
-          </el-col>
-        </el-row>
-      </content-warp>
-      <content-warp :name="$t('Will Return')" size="small" more-link="/show">
+      <content-warp name="即将回归" size="small" more-link="/show">
         <el-row>
           <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="8" v-for="(show,index) of returnShowList" :key="show.showId">
             <show-item shape="list" :info="show" :delay="index"/>
           </el-col>
         </el-row>
       </content-warp>
+      <content-warp name="高分推荐" size="small" more-link="/show">
+        <el-row>
+          <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="8" v-for="(show,index) of hotShowList" :key="show.showId">
+            <show-item shape="list" :info="show" :delay="index"/>
+          </el-col>
+        </el-row>
+      </content-warp>
     </el-col>
     <el-col :xs="9" :sm="9" :md="7" :lg="6" :xl="5" class="stick-top">
+      <content-warp name="本月热播" size="small" more-link="/show">
+        <show-item shape="list" v-for="(show,index) of monthShowList" :key="show.showId" :info="show" :delay="index"/>
+      </content-warp>
       <content-warp name="片单推荐" size="small" more-link="/album">
         <album-item v-for="album of recommendedAlbumList" :key="album.albumId" :info="album" :card="false"/>
       </content-warp>
@@ -43,22 +49,26 @@
 export default {
   data() {
     return {
-      returnShowList: [], // 即将回归
       hotShowList: [], // 热门
+      monthShowList: [], // 本月热播
+      returnShowList: [], // 即将回归
       recommendedAlbumList: [] // 推荐片单
     }
   },
   async asyncData({app}) {
     let [
+      {data: {data: monthShowList}},
       {data: {data: returnShowList}},
       {data: {data: hotShowList}},
       {data: {data: recommendedAlbumList}}
     ] = await Promise.all([
+      app.$axios.get('/show/month'),
       app.$axios.get('/show/return'),
       app.$axios.get('/show/hot'),
       app.$axios.get('/album/recommended')
     ])
     return {
+      monthShowList,
       returnShowList,
       hotShowList,
       recommendedAlbumList
